d3e086f34098141c3497bb35ce4be9e55ed8c74d
FacuDemarchi <f.dem1997@gmail.com>
2023-08-25 02:07:51 -0300

    Conectar api - definir funciones service comercios

--- src/app/comercios.service.ts
+++ src/app/comercios.service.ts
@@ -1,16 +1,20 @@
 // comercios.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ComerciosService {
-  getComercios(): any[] {
-    // Lógica para obtener la lista de comercios desde el backend o datos de ejemplo
-    return [
-      { nombre: 'Comercio 1', direccion: 'Dirección 1', telefono: '123456789' },
-      { nombre: 'Comercio 2', direccion: 'Dirección 2', telefono: '987654321' },
-      { nombre: 'Comercio 3', direccion: 'Dirección 3', telefono: '567891234' }
-    ];
+  private apiUrl = 'https://tu-api.com/comercios'; // Crear e implementar api
+
+  constructor(private http: HttpClient) {}
+  
+  obtenerListaComercios() {
+    return this.http.get(this.apiUrl);
+  }
+
+  obtenerDetalleComercio(id: any) {
   }
 }
